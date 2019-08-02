d9d9d57a31c4a0daac9e06c952f2f2dd8911fb81
chaodaiG <45011425+chaodaiG@users.noreply.github.com>
2019-08-02 13:30:45 -0700

    Use knative.dev/test-infra (#49)

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -19,7 +19,7 @@
 # For convenience, it can also be executed manually.
 
 export GO111MODULE=on
-source $(dirname $0)/../vendor/github.com/knative/test-infra/scripts/presubmit-tests.sh
+source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/presubmit-tests.sh
 
 # We use the default build, unit and integration test runners.
 
